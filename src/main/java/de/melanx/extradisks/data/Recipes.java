76d17acd1057fe49be1e1c184a735bdd0443ed71
MelanX <info@melanx.de>
2021-01-19 19:00:25 +0100

    change recipes to use tags

--- src/main/java/de/melanx/extradisks/data/Recipes.java
+++ src/main/java/de/melanx/extradisks/data/Recipes.java
@@ -11,7 +11,7 @@ import de.melanx.extradisks.items.item.ExtraItemStorageType;
 import net.minecraft.data.*;
 import net.minecraft.item.Item;
 import net.minecraft.item.crafting.Ingredient;
-import net.minecraft.item.crafting.ShapelessRecipe;
+import net.minecraft.tags.ITag;
 import net.minecraft.util.IItemProvider;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.Tags;
@@ -27,29 +27,29 @@ public class Recipes extends RecipeProvider {
     @Override
     protected void registerRecipes(@Nonnull Consumer<IFinishedRecipe> consumer) {
         for (ExtraItemStorageType type : ExtraItemStorageType.values()) {
-            this.registerDiskRecipes(Registration.ITEM_STORAGE_DISK.get(type).get(), Registration.ITEM_STORAGE_PART.get(type).get(), consumer);
-            this.registerStorageBlockRecipe(Registration.ITEM_STORAGE_PART.get(type).get(), Registration.ITEM_STORAGE_BLOCK.get(type).get(), consumer);
+            this.registerDiskRecipes(Registration.ITEM_STORAGE_DISK.get(type).get(), ModTags.Items.PARTS_ITEM.get(type), consumer);
+            this.registerStorageBlockRecipe(ModTags.Items.PARTS_ITEM.get(type), Registration.ITEM_STORAGE_BLOCK.get(type).get(), consumer);
         }
 
         for (ExtraFluidStorageType type : ExtraFluidStorageType.values()) {
-            this.registerDiskRecipes(Registration.FLUID_STORAGE_DISK.get(type).get(), Registration.FLUID_STORAGE_PART.get(type).get(), consumer);
-            this.registerStorageBlockRecipe(Registration.FLUID_STORAGE_PART.get(type).get(), Registration.FLUID_STORAGE_BLOCK.get(type).get(), consumer);
+            this.registerDiskRecipes(Registration.FLUID_STORAGE_DISK.get(type).get(), ModTags.Items.PARTS_FLUID.get(type), consumer);
+            this.registerStorageBlockRecipe(ModTags.Items.PARTS_FLUID.get(type), Registration.FLUID_STORAGE_BLOCK.get(type).get(), consumer);
         }
 
         this.registerPartRecipe(Registration.ITEM_STORAGE_PART.get(ExtraItemStorageType.TIER_5).get(), RSItems.ITEM_STORAGE_PARTS.get(ItemStorageType.SIXTY_FOUR_K).get(), consumer);
-        this.registerPartRecipe(Registration.ITEM_STORAGE_PART.get(ExtraItemStorageType.TIER_6).get(), Registration.ITEM_STORAGE_PART.get(ExtraItemStorageType.TIER_5).get(), consumer);
-        this.registerPartRecipe(Registration.ITEM_STORAGE_PART.get(ExtraItemStorageType.TIER_7).get(), Registration.ITEM_STORAGE_PART.get(ExtraItemStorageType.TIER_6).get(), consumer);
-        this.registerPartRecipe(Registration.ITEM_STORAGE_PART.get(ExtraItemStorageType.TIER_8).get(), Registration.ITEM_STORAGE_PART.get(ExtraItemStorageType.TIER_7).get(), consumer);
-        this.registerPartRecipe(Registration.ITEM_STORAGE_PART.get(ExtraItemStorageType.TIER_9).get(), Registration.ITEM_STORAGE_PART.get(ExtraItemStorageType.TIER_8).get(), consumer);
-        this.registerAdvancedPartRecipe(Registration.ITEM_STORAGE_PART.get(ExtraItemStorageType.TIER_10).get(), Registration.ITEM_STORAGE_PART.get(ExtraItemStorageType.TIER_9).get(), consumer);
-        this.registerAdvancedPartRecipe(Registration.ITEM_STORAGE_PART.get(ExtraItemStorageType.TIER_11).get(), Registration.ITEM_STORAGE_PART.get(ExtraItemStorageType.TIER_10).get(), consumer);
-        this.registerAdvancedPartRecipe(Registration.ITEM_STORAGE_PART.get(ExtraItemStorageType.TIER_12).get(), Registration.ITEM_STORAGE_PART.get(ExtraItemStorageType.TIER_11).get(), consumer);
+        this.registerPartRecipe(Registration.ITEM_STORAGE_PART.get(ExtraItemStorageType.TIER_6).get(), ModTags.Items.PARTS_ITEM.get(ExtraItemStorageType.TIER_5), consumer);
+        this.registerPartRecipe(Registration.ITEM_STORAGE_PART.get(ExtraItemStorageType.TIER_7).get(), ModTags.Items.PARTS_ITEM.get(ExtraItemStorageType.TIER_6), consumer);
+        this.registerPartRecipe(Registration.ITEM_STORAGE_PART.get(ExtraItemStorageType.TIER_8).get(), ModTags.Items.PARTS_ITEM.get(ExtraItemStorageType.TIER_7), consumer);
+        this.registerPartRecipe(Registration.ITEM_STORAGE_PART.get(ExtraItemStorageType.TIER_9).get(), ModTags.Items.PARTS_ITEM.get(ExtraItemStorageType.TIER_8), consumer);
+        this.registerAdvancedPartRecipe(Registration.ITEM_STORAGE_PART.get(ExtraItemStorageType.TIER_10).get(), ModTags.Items.PARTS_ITEM.get(ExtraItemStorageType.TIER_9), consumer);
+        this.registerAdvancedPartRecipe(Registration.ITEM_STORAGE_PART.get(ExtraItemStorageType.TIER_11).get(), ModTags.Items.PARTS_ITEM.get(ExtraItemStorageType.TIER_10), consumer);
+        this.registerAdvancedPartRecipe(Registration.ITEM_STORAGE_PART.get(ExtraItemStorageType.TIER_12).get(), ModTags.Items.PARTS_ITEM.get(ExtraItemStorageType.TIER_11), consumer);
 
         this.registerPartRecipe(Registration.FLUID_STORAGE_PART.get(ExtraFluidStorageType.TIER_5_FLUID).get(), RSItems.FLUID_STORAGE_PARTS.get(FluidStorageType.FOUR_THOUSAND_NINETY_SIX_K).get(), consumer);
-        this.registerPartRecipe(Registration.FLUID_STORAGE_PART.get(ExtraFluidStorageType.TIER_6_FLUID).get(), Registration.FLUID_STORAGE_PART.get(ExtraFluidStorageType.TIER_5_FLUID).get(), consumer);
-        this.registerPartRecipe(Registration.FLUID_STORAGE_PART.get(ExtraFluidStorageType.TIER_7_FLUID).get(), Registration.FLUID_STORAGE_PART.get(ExtraFluidStorageType.TIER_6_FLUID).get(), consumer);
-        this.registerAdvancedPartRecipe(Registration.FLUID_STORAGE_PART.get(ExtraFluidStorageType.TIER_8_FLUID).get(), Registration.FLUID_STORAGE_PART.get(ExtraFluidStorageType.TIER_7_FLUID).get(), consumer);
-        this.registerAdvancedPartRecipe(Registration.FLUID_STORAGE_PART.get(ExtraFluidStorageType.TIER_9_FLUID).get(), Registration.FLUID_STORAGE_PART.get(ExtraFluidStorageType.TIER_8_FLUID).get(), consumer);
+        this.registerPartRecipe(Registration.FLUID_STORAGE_PART.get(ExtraFluidStorageType.TIER_6_FLUID).get(), ModTags.Items.PARTS_FLUID.get(ExtraFluidStorageType.TIER_5_FLUID), consumer);
+        this.registerPartRecipe(Registration.FLUID_STORAGE_PART.get(ExtraFluidStorageType.TIER_7_FLUID).get(), ModTags.Items.PARTS_FLUID.get(ExtraFluidStorageType.TIER_6_FLUID), consumer);
+        this.registerAdvancedPartRecipe(Registration.FLUID_STORAGE_PART.get(ExtraFluidStorageType.TIER_8_FLUID).get(), ModTags.Items.PARTS_FLUID.get(ExtraFluidStorageType.TIER_7_FLUID), consumer);
+        this.registerAdvancedPartRecipe(Registration.FLUID_STORAGE_PART.get(ExtraFluidStorageType.TIER_9_FLUID).get(), ModTags.Items.PARTS_FLUID.get(ExtraFluidStorageType.TIER_8_FLUID), consumer);
 
         this.registerProcessorRecipe(Registration.WITHERING_PROCESSOR.get(), Registration.RAW_WITHERING_PROCESSOR.get(), Ingredient.fromTag(Tags.Items.NETHER_STARS), consumer);
 
@@ -103,7 +103,20 @@ public class Recipes extends RecipeProvider {
                 .build(consumer, new ResourceLocation(ExtraDisks.MODID, "part/" + result.getRegistryName().getPath()));
     }
 
-    private void registerAdvancedPartRecipe(Item result, Item prevPart, Consumer<IFinishedRecipe> consumer) {
+    private void registerPartRecipe(Item result, ITag.INamedTag<Item> prevPart, Consumer<IFinishedRecipe> consumer) {
+        ShapedRecipeBuilder.shapedRecipe(result)
+                .patternLine("DED")
+                .patternLine("PRP")
+                .patternLine("DPD")
+                .key('D', RSItems.PROCESSORS.get(ProcessorItem.Type.ADVANCED).get())
+                .key('E', RSItems.QUARTZ_ENRICHED_IRON.get())
+                .key('P', prevPart)
+                .key('R', Tags.Items.DUSTS_REDSTONE)
+                .addCriterion("has_prev_part", hasItem(prevPart))
+                .build(consumer, new ResourceLocation(ExtraDisks.MODID, "part/" + result.getRegistryName().getPath()));
+    }
+
+    private void registerAdvancedPartRecipe(Item result, ITag.INamedTag<Item> prevPart, Consumer<IFinishedRecipe> consumer) {
         ShapedRecipeBuilder.shapedRecipe(result)
                 .patternLine("DED")
                 .patternLine("PRP")
@@ -116,7 +129,7 @@ public class Recipes extends RecipeProvider {
                 .build(consumer, new ResourceLocation(ExtraDisks.MODID, "part/" + result.getRegistryName().getPath()));
     }
 
-    private void registerDiskRecipes(Item result, Item part, Consumer<IFinishedRecipe> consumer) {
+    private void registerDiskRecipes(Item result, ITag.INamedTag<Item> part, Consumer<IFinishedRecipe> consumer) {
         ShapedRecipeBuilder.shapedRecipe(result)
                 .patternLine("GEG")
                 .patternLine("EPE")
@@ -135,7 +148,7 @@ public class Recipes extends RecipeProvider {
                 .build(consumer, new ResourceLocation(ExtraDisks.MODID, "disk/shapeless/" + result.getRegistryName().getPath()));
     }
 
-    private void registerStorageBlockRecipe(IItemProvider part, IItemProvider block, Consumer<IFinishedRecipe> consumer) {
+    private void registerStorageBlockRecipe(ITag.INamedTag<Item> part, IItemProvider block, Consumer<IFinishedRecipe> consumer) {
         ShapedRecipeBuilder.shapedRecipe(block)
                 .patternLine("QPQ")
                 .patternLine("QCQ")
